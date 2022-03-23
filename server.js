225b4656bd10344fef4d6b5162170556dec86bba
gustavnun1 <gustavo.nunes0422@gmail.com>
2022-03-23 11:20:37 -0300

    mudando a responsividade

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const server = express();
+
+const nunjucks = require("nunjucks");
+
+//Cofigurar rota para o express encontrar arquivos estáticos(css, imgs, scripts)
+server.use(express.static("public"))
+
+//Configurando o nunjucks
+nunjucks.configure("views", {
+    express: server,
+    noCache: true,
+})
+
+server.get("/",function(req, res){
+    console.log("Métis");
+    return res.render("metis.html", { metis });
+});
+
+
+// 2 - Rota para pag projetos (/projetos)
+server.get("/login", function(req, res){
+    return res.render("login.html", { login });
+});
+
+server.get("/Sobre Nós",function(req, res){
+    return res.render("sobrenos.html", { sobre });
+});
+
+//"Ligamos" nosso servidor na porta 3001
+server.listen(5500);
