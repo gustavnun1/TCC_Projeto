6a13894a6028fcab91322778caf7d5b04e6a391e
gustavnun1 <gustavo.nunes0422@gmail.com>
2022-03-31 12:44:58 -0300

    Inserij

--- main.js
+++ main.js
@@ -1,15 +1,37 @@
-
+const button = document.getElementById('button');
+const email = document.getElementById('inpemail');
+const senha = document.getElementById('inpsenha');
+const usuario = document.getElementById('inusuario');
+const confirmado = document.getElementById('inpconfirmar')
 function enviarTexto (){
-    const button = document.getElementById("button");
-    const input1 = document.getElementById("inpconfirmar")
-    if(input1.value == ""){
+   
+
+    if(email.value == ""){
         alert('Complete todas as caixas corretamente');
-        return;
-
-
-    }else{
-        alert('Verifique se preencheu as caixas corretamente')
+        return;   
+    }
+    
+    else if(senha.value == ""){
+        alert('Complete todas as caixas corretamente');
+        return;   
+    }
+    else if(usuario.value == ""){
+        alert('Complete todas as caixas corretamente');
+        return;   
+    }
+    else if(confirmado.value == ""){
+        alert('Complete todas as caixas corretamente');
+        return;   
+    }
+    
+    else{
+        alert('Você foi registrado! Seu perfil está disponivel para personalização 😀');
+    
+            window.location.href = "./login.html";
+        ;
     }
 
 
 }
+
+button.addEventListener('click', enviarTexto, true);
