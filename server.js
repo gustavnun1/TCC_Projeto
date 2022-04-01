56da300c18a0ee002d727f92e23f4f6ada7b61cc
gustavnun1 <gustavo.nunes0422@gmail.com>
2022-04-01 13:30:43 -0300

    Inserindo JS

--- server.js
+++ server.js
@@ -1,31 +1,10 @@
-const express = require("express");
-const server = express();
+const btnMobile = document.getElementById('imagemS');
 
-const nunjucks = require("nunjucks");
+function toogleMenu(){
+    
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
 
-//Cofigurar rota para o express encontrar arquivos estáticos(css, imgs, scripts)
-server.use(express.static("public"))
+}
 
-//Configurando o nunjucks
-nunjucks.configure("views", {
-    express: server,
-    noCache: true,
-})
-
-server.get("/",function(req, res){
-    console.log("Métis");
-    return res.render("metis.html", { metis });
-});
-
-
-// 2 - Rota para pag login (/login)
-server.get("/login", function(req, res){
-    return res.render("login.html", { login });
-});
-
-server.get("/Sobre Nós",function(req, res){
-    return res.render("sobrenos.html", { sobre });
-});
-
-//"Ligamos" nosso servidor na porta 3001
-server.listen(5500);
+btnMobile.addEventListener('click', toogleMenu, true);
